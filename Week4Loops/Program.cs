bce1a696617a51f6d525cfbb8c795a102020beb7
Anna Antipova <aantipov@tktk.ee>
2023-05-06 16:09:41 +0300

    create new app

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿//FOR-LOOP
+//DRY-principle do not repeat Yourself
+
+
+
+
+int i = 0;
+
+for( i = 0; i < 5; i++) // i++ = i+1
+{
+    Console.WriteLine("We do not need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+//Console.Writeline("We do not need no education.");
+//Console.Writeline("We do not need no education.");
+//Console.Writeline("We do not need no education.");
+// Console.Writeline("We do not need no education.");
+// Console.Writeline+0("We do not need no education.");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿Random rnd = new Random();
+int cpuRandom;
+
+bool LoopActive = true; //bool-true/false
+
+while(LoopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess.Enter a number 1-3");
+    int UserNumber = Int32.Parse(Console.ReadLine());
+
+    if (UserNumber == cpuRandom)
+  {
+        Console.WriteLine("Congratulations, you won!");
+        //LoopActive = false;
+        break;
+  } 
+    else
+  {
+        Console.WriteLine("Oops. Try again.");
+  }
+ }
+Console.WriteLine("Have a nice day!");
